e942f3000e17ec0636d9b5cd3c77d2d9ad21b0c9
lixinjiang <lixinjiang@duxiaoman.com>
2020-04-30 18:10:18 +0800

    exclude scala-api

--- src/main/java/cn/lixinjiang/flinkdemo/tablesql/TableStreamSource.java
+++ src/main/java/cn/lixinjiang/flinkdemo/tablesql/TableStreamSource.java
@@ -14,6 +14,12 @@ import cn.lixinjiang.flinkdemo.streamsource.Item;
  */
 public class TableStreamSource implements SourceFunction<Item> {
 
+    /**
+     * 根据类名、接口名、成员方法及属性等来生成一个64位的哈希字段   默认为 1L
+     * 反序列标识，反序列化过程中，如果字节流中的serialVersionUID本本地实体类的serialVersionUID一直，则可进行反序列化，否则不可
+     */
+    private static final long serialVersionUID = -7516444794187889441L;
+
     private boolean isRunning = true;
 
     @Override
