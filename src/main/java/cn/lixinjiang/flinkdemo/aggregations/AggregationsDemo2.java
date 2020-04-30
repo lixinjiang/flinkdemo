a78a46ef4df2a63d570b8896c829e0515132a6ec
lixinjiang <lixinjiang@duxiaoman.com>
2020-04-30 15:59:58 +0800

    update .

--- src/main/java/cn/lixinjiang/flinkdemo/aggregations/AggregationsDemo2.java
+++ src/main/java/cn/lixinjiang/flinkdemo/aggregations/AggregationsDemo2.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -25,16 +26,13 @@ public class AggregationsDemo2 {
         data.add(new Tuple3<>(1, 2, 13));
         DataStreamSource<Tuple3<Integer, Integer, Integer>> items = env.fromCollection(data);
 
-        items.keyBy(0).reduce(new ReduceFunction<Tuple3<Integer, Integer, Integer>>() {
-            @Override
-            public Tuple3<Integer, Integer, Integer> reduce(
-                    Tuple3<Integer, Integer, Integer> t1, Tuple3<Integer, Integer, Integer> t2)
-                    throws Exception {
-                Tuple3<Integer, Integer, Integer> newTuple = new Tuple3<>();
-                newTuple.setFields(0, 0, (Integer) t1.getField(2) + (Integer) t2.getField(2));
-                return newTuple;
-            }
-        }).printToErr().setParallelism(1);
+        SingleOutputStreamOperator<Tuple3<Integer, Integer, Integer>> reduce =
+                items.keyBy(0).reduce((ReduceFunction<Tuple3<Integer, Integer, Integer>>) (t1, t2) -> {
+                    Tuple3<Integer, Integer, Integer> newTuple = new Tuple3<>();
+                    newTuple.setFields(0, 0, (Integer) t1.getField(2) + (Integer) t2.getField(2));
+                    return newTuple;
+                });
+        reduce.printToErr().setParallelism(1);
 
         env.execute();
     }
